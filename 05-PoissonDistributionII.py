17184ae676dd268f537f5165405bf39b15b57647
Cesar Bhering <cesar.bhering@ericsson.com>
2020-06-26 15:37:39 -0300

    Desafios Dia 05

---
+++ 05-PoissonDistributionII.py
@@ -0,0 +1,11 @@
+#Na Parte de Discussões tem uma boa explicação estatística sobre a fórmula, no 1º Post.
+
+#Maquina A
+x = 0.88
+CustoA = 160+40*(x+x**2)
+print(round(CustoA,3))
+
+#Maquina B
+y = 1.55
+CustoB = 128+40*(y+y**2)
+print(round(CustoB,3))
